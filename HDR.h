d2e8f0ebd90c73966e0c7bdca543e74161504b88
gremaxboy <gremaxboy@users.noreply.github.com>
2024-08-02 19:14:36 +0300

    new commit

--- HDR.h
+++ HDR.h
@@ -1,4 +1,4 @@
-﻿#ifndef _HDR_H
+#ifndef _HDR_H
 #define _HDR_H
 #include <opencv2/opencv.hpp>
 #include<opencv2/core/core.hpp>
@@ -24,16 +24,17 @@ Type stringToNum(const string& str)
 	Type num;
 	iss >> num;
 	return num;
+	//dfsdfsd
 }
 template<typename T> string toString(const T& t)
 {
-	ostringstream oss;  //创建一个格式化输出流
-	oss << t;             //把值传递如流中
+	ostringstream oss;  //
+	oss << t;             //
 	return oss.str();
 }
-//搜索路劲由IMAGE_SEARCH_PATH制定。//如IMAGE_SEARCH_PATH="inputimage/8/*.bmp"字符串里注意不要出现空格！！！
-//ReadSequenceImage:在某文件夹内读取图片的程序，不需要在意图片名字、数量等，
-//但char * IMAGE_SEARCH_PATH 后缀名如果多于3个字母则需要修改函数
+//IMAGE_SEARCH_PATH//IMAGE_SEARCH_PATH="inputimage/8/*.bmp"
+//ReadSequenceImage:
+//char * IMAGE_SEARCH_PATH 3
 void ReadSequenceImage( vector<Mat>&images);
 #endif
 //MAT.TYPE=1:8UC1 5:32FC1
